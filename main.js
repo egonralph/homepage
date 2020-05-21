db8d5824c4c407aba36f80912dd280bc222611c0
Rafal Duma <egonralf@poczta.onet.pl>
2020-05-21 20:24:03 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+var name = 'Rafał';
+var lastName = 'Duma';
+var year = 2016;
+var userName = 'Frankie';
+
+console.log(`Witaj ${userName}, jak się masz? Nazywam się ${name} ${lastName} i uczę sie frontedu od ${year} roku`);
