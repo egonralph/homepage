2c63bce1a126bc6f2359583f1e2b446a1b313e67
Rafal Duma <egonralf@poczta.onet.pl>
2020-05-22 12:49:16 +0200

    Added new css classes, create new paragraph using JS (innerHTMLfunction)

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-var name = 'Rafał';
-var lastName = 'Duma';
-var year = 2016;
-var userName = 'Frankie';
+const name = 'Rafał';
+const lastName = 'Duma';
+const year = 2016;
+const userName = 'Frankie';
+
+
+
+const heading = document.querySelector('.section__paragraph--js');
+heading.innerHTML = 'Witaj ' + userName + ', jak się masz? Nazywam się ' + name + ' ' + lastName + ' i uczę sie frontedu od ' + year + ' roku.'
+
 
 console.log(`Witaj ${userName}, jak się masz? Nazywam się ${name} ${lastName} i uczę sie frontedu od ${year} roku`);
