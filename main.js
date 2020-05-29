5926edf0a41a837ab9a50c10c29f0933469465ee
Rafal Duma <egonralf@poczta.onet.pl>
2020-05-29 19:50:03 +0200

    Changed page content, added new js functionality

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 const name = 'Rafał';
 const lastName = 'Duma';
+const age = 38;
 const year = 2016;
 const userName = 'Frankie';
 
-
-
 const heading = document.querySelector('.section__paragraph--js');
-heading.innerHTML = 'Witaj ' + userName + ', jak się masz? Nazywam się ' + name + ' ' + lastName + ' i uczę sie frontedu od ' + year + ' roku.'
+heading.innerHTML = 'Witaj ' + userName + ', jak się masz? Nazywam się ' + name + ' ' + lastName + ' i zajmuję sie frontedem od ' + year + ' roku.'
 
 
-console.log(`Witaj ${userName}, jak się masz? Nazywam się ${name} ${lastName} i uczę sie frontedu od ${year} roku`);
+console.log(`Witaj ${userName}, jak się masz? Nazywam się ${name} ${lastName} i zajmuję sie frontedem od ${year} roku`);
+
+function welcomeOld(age, name, lastName) {
+    console.log(`Czesc, Nazywam się ${name} ${lastName} i mam ${age} lat`);
+}
+welcomeOld(15, 'Tomek', 'Sawyer');
+
+const welcome = (age, name, lastName) =>{
+    console.log(`Czesc, Nazywam się ${name} ${lastName} i mam ${age} lat`);
+}
+welcomeOld(24, 'Marysia', 'Konopnicka');
+
+function addContent(querySelectorPage, contentPage) {
+    const element = document.querySelector(querySelectorPage);
+    element.innerHTML = contentPage;
+}
+
+addContent('.section__paragraph--js','Witaj na mojej stronie');
